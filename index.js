132aa96544b14f5217b423361d7cef34efc0f5aa
montrell43 <tmerriweather@persevernow.org>
2025-04-29 10:35:31 -0500

    Serve full package.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/package.json', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Jane Doe');
 });
 
