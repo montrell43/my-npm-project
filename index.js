5efbdf8cc1e3a4c36b9970b216b84842195f20fe
montrell43 <tmerriweather@persevernow.org>
2025-04-29 10:14:54 -0500

    Add /package.json route for freeCodeCamp test

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from Render!');
 });
 
+// Add this:
+app.get('/package.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'package.json'));
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
